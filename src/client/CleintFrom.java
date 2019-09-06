2ecb4b65aa841dcb9dea7688a29f0b8c01913a8b
Rajul Vishwakarma <rajulvishwakarma1@gmail.com>
2019-09-06 21:51:05 +0530

    Client Form is Completed check in Clint Directory.

--- src/client/CleintFrom.java
+++ src/client/CleintFrom.java
@@ -1,4 +1,63 @@
 package client;
 
-public class CleintFrom {
+import utility.ClientData;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class CleintFrom extends JFrame {
+    JLabel L1,L2,L3;
+    JTextField t1,t2,t3;
+    JButton b1;
+    JPanel p1;
+
+    public CleintFrom(){
+        setVisible(true);
+        setSize(300,200);
+        setTitle("Client Form");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        p1 = new JPanel();
+        L1 = new JLabel("Email");
+        L2 = new JLabel("Name");
+        L3 = new JLabel("Mobile");
+        t1 = new JTextField(20);
+        t2 = new JTextField(20);
+        t3 = new JTextField(20);
+        b1 = new JButton("Submit");
+        p1.add(L1);
+        p1.add(t1);
+        p1.add(L2);
+        p1.add(t2);
+        p1.add(L3);
+        p1.add(t3);
+        p1.add(b1);
+        getContentPane().add(p1);
+
+        p1.setBorder(BorderFactory.createTitledBorder("Client From"));
+        b1.addActionListener(e->b1Click(e));
+
+    }
+    public void b1Click(ActionEvent e){
+        try{
+            Socket s = new Socket("localhost",8880);
+            ObjectOutputStream ois = new ObjectOutputStream(s.getOutputStream());
+            ClientData obj = new ClientData();
+            obj.email = t1.getText();
+            obj.name = t1.getText();
+            obj.mobile = t1.getText();
+            ois.writeObject(obj);
+            ois.close();
+            s.close();
+            JOptionPane.showMessageDialog(null,"Data Sent");
+        }
+        catch (Exception ex){
+            System.out.println(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        new CleintFrom();
+    }
 }
---
+++ src/utility/ClientData.java
@@ -0,0 +1,8 @@
+package utility;
+
+public class ClientData {
+    public String email;
+    public String name;
+    public String mobile;
+
+}
