79e6232538dbebdd17cd685aca9ae131f240c959
Rajul Vishwakarma <rajulvishwakarma1@gmail.com>
2019-09-06 22:01:29 +0530

    Client Form is Completed check in Clint Directory.

--- src/server/AppServer.java
+++ src/server/AppServer.java
@@ -1,4 +1,51 @@
 package server;
 
-public class AppServer {
+import utility.ClientData;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class AppServer implements Runnable {
+    ServerSocket ss;
+    Socket s;
+    Thread t1;
+    public AppServer(){
+        try{
+            ss = new ServerSocket(8880);
+            t1 = new Thread(this);
+            t1.start();
+            System.out.println("Server Started.....");
+        }
+        catch (Exception ex){
+            System.out.println(ex);
+        }
+    }
+    public void run(){
+        while (true){
+            try {
+                s = ss.accept();
+                Connect C = new Connect(s);
+            }
+            catch (Exception ex){
+
+            }
+        }
+    }
+    class Connect{
+        Connect(Socket s){
+            try{
+                ObjectInputStream oin = new ObjectInputStream(s.getInputStream());
+                ClientData obj= new ClientData();
+                System.out.println("Email :"+obj.email);
+                System.out.println("Name :"+obj.name);
+                System.out.println("Mobile :"+obj.mobile);
+            }
+            catch (Exception ex){
+
+            }
+
+        }
+    }
 }
